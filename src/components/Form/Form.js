c7a9fe376d681806bdc2b53c4740e772d35ee7de
Khrystyna Halaiko <gayka1818@gmail.com>
2022-12-07 13:17:56 +0200

    Add

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,7 +7,14 @@ export class Form extends React.Component {
   state = { 
     name: '',
     number: '',
-   };
+  };
+  
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
   
   handleSubmit = event => {
     event.preventDefault();
@@ -29,21 +36,25 @@ export class Form extends React.Component {
         <label>
           Name
           <input className={css.input}
+            value={this.state.name}
             type="text"
             name='name'
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            onChange={this.handleChange}
           />
         </label>
         <label>
           Number
           <input className={css.input}
+            value={this.state.number}
             type="tel"
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+            onChange={this.handleChange}
           />
         </label>
 
